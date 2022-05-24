61ec147cf3a9f5f6ac8d75e4f396e647cd89d8d7
Mehdi BENNOUNA <Bennouna.Mehdi@Hotmail.com>
2022-05-24 10:53:36 +0100

    setup book list page layout and style

---
+++ books.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include_once "./includes/head.php" ?>
+	<link rel="stylesheet" href="./css/books.css">
+	<title>Infinity Book | Books</title>
+</head>
+
+<body>
+	<?php include_once "./includes/nav.php" ?>
+	<main>
+		<div class="grid-container">
+			<div class="book-container">
+				<a href="#">
+					<img src="./img/4671.jpg" alt="book-image">
+				</a>
+				<h3>The Great Gatsby</h3>
+			</div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+			<div class="book-container"></div>
+		</div>
+	</main>
+	<?php include_once "./includes/footer.php" ?>
+</body>
+
+</html>
